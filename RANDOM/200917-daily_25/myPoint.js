5a35da2b4055f087a6e2856564e6c878e056418c
Mindaugas Dudenas <murama@Mindaugass-MacBook-Pro.local>
2020-09-17 16:30:25 +0300

    25

---
+++ RANDOM/200917-daily_25/myPoint.js
@@ -0,0 +1,45 @@
+const _totaltrail = 32
+
+//————————————————————————————————————————————— myPoint 
+class myPoint {
+  constructor(idx, x, y) {
+    if (idx != -1) {
+      this.idx = idx;
+      this.pos = createVector(x, y, 0)
+      this.trail = []
+      this.totalTrail = floor(random(1, _totaltrail))
+      this.opos = []
+      this.curr = 0
+    }
+  }
+
+
+  //————————————————————————————————————————————— myPoint show
+  update() {
+
+    const fcount = floor((frameCount + this.idx * 0.05) % _totalFrames)
+    if (fcount % _totalFrames == 0) this.curr = (this.curr + 1) % _letters.length
+    let percent = (fcount) / _totalFrames;
+    percent = ease[styles[2]](percent);
+
+    // let nforce = map(openSimplex.noise2D(percent * TWO_PI, 0), -1, 1, -24, 24)
+
+    this.pos.x = lerp(this.pos.x, map(percent, 0, 1, this.opos[this.curr].x, this.opos[(this.curr + 1) % _letters.length].x), 1.)
+    this.pos.y = lerp(this.pos.y, map(percent, 0, 1, this.opos[this.curr].y, this.opos[(this.curr + 1) % _letters.length].y), 1.)
+
+    if (this.trail.length > this.totalTrail) {
+      this.trail.splice(0, 1)
+    }
+
+
+    this.trail.push(this.pos.copy())
+  }
+
+  //————————————————————————————————————————————— myPoint show
+  show() {
+    const elm = this.trail[0]
+    ellipse(elm.x, elm.y, 1, 1)
+    const elm_b = this.trail[this.trail.length - 1]
+    vertex(elm_b.x, elm_b.y)
+  }
+}
